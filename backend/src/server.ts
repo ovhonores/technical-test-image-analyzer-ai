87665c11144c63cb4d36821d28d6dac40523ad61
orlando.honores <orlando.honores@pervasivemin.net>
2025-11-21 04:11:47 -0500

    chore: setup docker compose for backend and frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const PORT = process.env.PORT || 3050;
 
 app.listen(PORT, () => {
-  console.log(`Runging on port: ${PORT}`);
+  console.log(`Runing on port: ${PORT}`);
 });
